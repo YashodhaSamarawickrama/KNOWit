e27a15ea9a37fcf8c666ed70ffaccc9c9d0d36b6
YashodhaSamarawickrama <yasho.samarawickrama@gmail.com>
2018-04-06 16:02:07 +0530

    server.js updated-KNOWit

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-var express = require('express');
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = 8800;
+
+app.listen(port,()=>{
+    console.log("Server is up and running on http://www.localhost: "+port);
+
+})
